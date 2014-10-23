30cab4bb81d5958bc5d5b7cae9ad3ef648a8a65c
julesy <jules89@arcor.de>
2014-10-23 07:38:32 +0200

    - added documentation to the ActionTimer - just added the given heuristics to our controller

--- src/ActionTimer.java
+++ src/ActionTimer.java
@@ -11,6 +11,12 @@ import tools.ElapsedCpuTimer;
  */
 public class ActionTimer {
 
+	// variable for defining a safety time limit
+	public int timeRemainingLimit = 3;
+
+	// multiplicator of the average time
+	public double timeAvgMultiplicator = 1.4;
+
 	// accumulated time
 	protected double acumTime;
 
@@ -23,12 +29,6 @@ public class ActionTimer {
 	// timer for the act method. is need for the remaining time
 	protected ElapsedCpuTimer actTimer;
 
-	// variable for defining a safety time limit
-	protected int TIME_REMAINING_LIMIT = 3;
-
-	// multiplicator of the average time
-	protected double TIME_AVG_MULTIPLICATOR = 1.4;
-
 	/**
 	 * This is the constructor for the Timer object. The elapsedTimer that is
 	 * given to the act method of the controller is needed to get the remaining
@@ -39,7 +39,8 @@ public class ActionTimer {
 	ActionTimer(ElapsedCpuTimer elapsedTimer) {
 		tmpTimer = null;
 		actTimer = elapsedTimer;
-		reset();
+		acumTime = 0;
+		numIters = 0;
 	}
 
 	void start() {
@@ -49,11 +50,6 @@ public class ActionTimer {
 	void stop() {
 		++this.numIters;
 		this.acumTime += (this.tmpTimer.elapsedMillis());
-	}
-
-	void reset() {
-		acumTime = 0;
-		numIters = 0;
 	}
 
 	/**
@@ -79,8 +75,8 @@ public class ActionTimer {
 	 * @return if there is enough time for a further iteration.
 	 */
 	boolean isTimeLeft() {
-		return getRemaining() >= TIME_AVG_MULTIPLICATOR * getAVG()
-				&& getRemaining() > TIME_REMAINING_LIMIT;
+		return getRemaining() >= timeAvgMultiplicator * getAVG()
+				&& getRemaining() > timeRemainingLimit;
 	}
 
 	/**
--- src/Agent.java
+++ src/Agent.java
@@ -1,65 +1,60 @@
 package controllers.cig;
 
-import java.util.ArrayList;
-import java.util.Random;
 
 import ontology.Types;
 import tools.ElapsedCpuTimer;
+import controllers.cig.heuristics.SimpleStateHeuristic;
 import core.game.StateObservation;
 import core.player.AbstractPlayer;
-
 
 public class Agent extends AbstractPlayer {
 
 
-	protected Random randomGenerator;
-	
+	protected ActionTimer timer;
 
-	
-	public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer)
-    {
-        randomGenerator = new Random();
-    }
-	
+	public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) {}
 
-    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-    	
-    	// Initialize the timer for all iterations that are made
-    	ActionTimer timer = new ActionTimer(elapsedTimer);
-    	
-    	// The action we will finally execute
-        Types.ACTIONS action = null;
-        
-        // copy of our state object
-        StateObservation stCopy = stateObs.copy();
-        
-        
-        // check whether we've enough time for a next iteration. 
-        System.out.println(timer.isTimeLeft());
-        
-        while(timer.isTimeLeft())
-        {
-        	timer.start();
-        	
-        	
-        	ArrayList<Types.ACTIONS> actions = stateObs.getAvailableActions();
-            int index = randomGenerator.nextInt(actions.size());
-            action = actions.get(index);
-            
-            stCopy.advance(action);
-            if(stCopy.isGameOver())
-            {
-                stCopy = stateObs.copy();
-            }
-            
-            
-            
-            timer.stop();
-        }
+	public Types.ACTIONS act(StateObservation stateObs,
+			ElapsedCpuTimer elapsedTimer) {
 
-        
-        
-        return action;
-    }
+		// Initialize the timer for all iterations that are made
+		timer = new ActionTimer(elapsedTimer);
+
+		// The action we will finally be executed
+		Types.ACTIONS bestAction = null;
+
+		// copy of our state object
+		StateObservation stCopy = null;
+
+		// initialize the values for the heuristic
+		double maxQ = Double.NEGATIVE_INFINITY;
+		SimpleStateHeuristic heuristic = new SimpleStateHeuristic(stateObs);
+
+		// search for the next best action
+		for (Types.ACTIONS action : stateObs.getAvailableActions()) {
+
+			// check whether we've enough time for a next iteration.
+			if (timer.isTimeLeft()) {
+				timer.start();
+				
+				stCopy = stateObs.copy();
+				stCopy.advance(action);
+				double Q = heuristic.evaluateState(stCopy);
+
+				if (Q > maxQ) {
+					maxQ = Q;
+					bestAction = action;
+				}
+
+				
+				timer.stop();
+			}
+
+		}
+
+		System.out.println(timer.status());
+		return bestAction;
+
+	}
 
 }
